ad0ac281a5c71b79b407ec8c87bb9db71bccd811
ne745 <kkpma0@gmail.com>
2022-03-19 23:38:52 +0900

    データの整形

--- src/finance/finance.py
+++ src/finance/finance.py
@@ -2,7 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
 
+# 読み込み
 aapl = yf.Ticker('AAPL')
 days =  20
 hist = aapl.history(period=f'{days}d')
-print(hist)
+
+# 整形
+hist.index = hist.index.strftime('%d %B %Y')
+hist = hist[['Close']]
+hist.columns = ['apple']
+hist = hist.T
+hist.index.name = "Name"
+print(hist.head(5))
