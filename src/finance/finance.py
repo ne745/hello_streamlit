b306d45583e7ae38cfc59d5756d1a6f1593d4fbb
ne745 <kkpma0@gmail.com>
2022-03-24 23:44:33 +0900

    streamlitに実装

--- src/finance/finance.py
+++ src/finance/finance.py
@@ -4,6 +4,28 @@ import functools
 import pandas as pd
 import yfinance as yf
 import altair as alt
+import streamlit as st
+
+st.title('米国株価可視化アプリ')
+
+# ---------- サイド ----------
+st.sidebar.write("""
+# 株価
+こちらは株価可視化ツールです．
+以下のオプションから表示日数を指定してください．
+""")
+
+st.sidebar.write("""
+## 表示日数選択
+""")
+days = st.sidebar.slider('日数', 1, 50, 20)
+# --------------------------
+
+# ---------- メイン ----------
+st.write(f"""
+### 過去 **{days}日間** の株価
+""")
+# --------------------------
 
 # timeout 設定
 # https://github.com/ranaroussi/yfinance/issues/539
@@ -16,6 +38,7 @@ yf.base._requests = session
 yf.utils._requests = session
 yf.ticker._requests = session
 
+@st.cache
 def get_data(days, tickers):
     df = pd.DataFrame()
     for company in tickers.keys():
@@ -27,12 +50,19 @@ def get_data(days, tickers):
         hist = hist[['Close']]
         hist.columns = [company]
         hist = hist.T
-        hist.index.name = "Name"
+        hist.index.name = 'Name'
         # 格納
         df = pd.concat([df, hist])
     return df
 
-days =  10
+# ---------- サイド ----------
+st.sidebar.write("""
+## 株価の範囲指定
+""")
+ymin, ymax = st.sidebar.slider('範囲', 0, 3500, (0, 3500))
+# --------------------------
+
+# 株価情報の取得
 tickers = {
     'google': 'GOOGL',
     'amazon': 'AMZN',
@@ -43,22 +73,38 @@ tickers = {
 }
 df = get_data(days, tickers)
 
-companies = ['google', 'amazon', 'facebook', 'apple', 'microsoft', 'netflix']
-data = df.loc[companies]
-data = data.sort_index()
-data = data.T.reset_index()
-data = pd.melt(data, id_vars=['Date']).rename(columns={'value':'Stock Prices(USD)'})
-
-# グラフ描画
-ymin, ymax = 200, 500
-chart = (
-    alt.Chart(data)
-    .mark_line(opacity=0.8, clip=True)
-    .encode(
-        x='Date:T',
-        y=alt.Y('Stock Prices(USD):Q', stack=None, scale=alt.Scale(domain=[ymin, ymax])),
-        color='Name:N'
-    )
+# ---------- メイン ----------
+companies = st.multiselect(
+    '会社を選択',
+    tickers.keys()
 )
+# --------------------------
 
-chart.show()
+if not companies:
+    st.error('少なくとも一社は選んでください')
+else:
+    data = df.loc[companies]
+    data = data.sort_index()
+
+    # ---------- メイン ----------
+    # テーブル表示
+    st.write("### 株価 (USD)", data)
+    # --------------------------
+
+    data = data.T.reset_index()
+    data = pd.melt(data, id_vars=['Date']).rename(columns={'value':'Stock Prices(USD)'})
+
+    # グラフ描画
+    chart = (
+        alt.Chart(data)
+        .mark_line(opacity=0.8, clip=True)
+        .encode(
+            x='Date:T',
+            y=alt.Y('Stock Prices(USD):Q', stack=None, scale=alt.Scale(domain=[ymin, ymax])),
+            color='Name:N'
+        )
+    )
+
+    # ---------- メイン ----------
+    st.altair_chart(chart, use_container_width=True)
+    # --------------------------
