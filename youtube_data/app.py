5255184d50ff7ff0f89aa9df82325c3690af51c2
ne745 <kkpma0@gmail.com>
2022-04-03 22:21:32 +0900

    登録者数を取得

--- youtube_data/app.py
+++ youtube_data/app.py
@@ -26,23 +26,53 @@ class YouTubeData(object):
             maxResults=max_results
         ).execute()
 
-        items = response['items']
         items_id = []
-        for item in items:
+        for item in response['items']:
             item_id = {}
             item_id['video_id'] = item['id']['videoId']
             item_id['channel_id'] = item['snippet']['channelId']
             items_id.append(item_id)
 
-        df_video = pd.DataFrame(items_id)
-        print(df_video)
+        self.df_video = pd.DataFrame(items_id)
+
+    def channel_subscriber(self):
+        channel_ids = self.df_video['channel_id'].unique().tolist()
+
+        response = self.youtube.channels().list(
+            id=','.join(channel_ids),
+            part='statistics',
+            fields='items(id,statistics(subscriberCount))'
+        ).execute()
+
+        items_id = []
+        for item in response['items']:
+            item_id = {}
+            item_id['channel_id'] = item['id']
+            try:
+                item_id['subscribers'] = int(item['statistics']['subscriberCount'])
+            except KeyError:
+                item_id['subscribers'] = 0
+            items_id.append(item_id)
+
+        self.df_subscribers = pd.DataFrame(items_id)
+
+    def merge_video_info(self):
+        self.df = pd.merge(left=self.df_video, right=self.df_subscribers, on='channel_id')
+
+    def extract_data(self):
+        self.extracted_df = self.df[self.df['subscribers'] < 5000]
+        print(self.extracted_df)
+
 
 def main():
     q = 'Python 自動化'
-    max_results = 10
+    max_results = 50
 
     youtube_data = YouTubeData()
     youtube_data.search_video(q, max_results)
+    youtube_data.channel_subscriber()
+    youtube_data.merge_video_info()
+    youtube_data.extract_data()
 
 if __name__ == '__main__':
     main()
