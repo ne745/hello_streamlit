2e441b0fe98270032382144928b77712d2e581ac
ne745 <kkpma0@gmail.com>
2022-04-03 17:53:15 +0900

    必要な処理をクラス化

--- txt2speech/hello_world.py
+++ txt2speech/hello_world.py
@@ -1,7 +1,6 @@
 # https://cloud.google.com/text-to-speech/docs/libraries?hl=JA
 
 import os
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './data/secret.json'
 
 """Synthesizes speech from the input string of text or ssml.
 Make sure to be working in a virtual environment.
@@ -11,32 +10,65 @@ Note: ssml must be well-formed according to:
 """
 from google.cloud import texttospeech
 
-# Instantiates a client
-client = texttospeech.TextToSpeechClient()
+class Txt2Speech(object):
+    def __init__(self) -> None:
+        self.lang_code = {
+            '日本語': 'ja-JP',
+            'English': 'en-US'
+        }
 
-# Set the text input to be synthesized
-synthesis_input = texttospeech.SynthesisInput(text="Hello, World!")
+        self.gender_type = {
+            'default': texttospeech.SsmlVoiceGender.SSML_VOICE_GENDER_UNSPECIFIED,
+            'male': texttospeech.SsmlVoiceGender.MALE,
+            'female': texttospeech.SsmlVoiceGender.FEMALE,
+            'neutral': texttospeech.SsmlVoiceGender.NEUTRAL
+        }
 
-# Build the voice request, select the language code ("en-US") and the ssml
-# voice gender ("neutral")
-voice = texttospeech.VoiceSelectionParams(
-    language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
-)
+        # Instantiates a client
+        self.client = texttospeech.TextToSpeechClient()
 
-# Select the type of audio file you want returned
-audio_config = texttospeech.AudioConfig(
-    audio_encoding=texttospeech.AudioEncoding.MP3
-)
+    def synthesize_speech(self, text, lang='日本語', gender='default'):
 
-# Perform the text-to-speech request on the text input with the selected
-# voice parameters and audio file type
-response = client.synthesize_speech(
-    input=synthesis_input, voice=voice, audio_config=audio_config
-)
+        # Set the text input to be synthesized
+        synthesis_input = texttospeech.SynthesisInput(text=text)
 
-# The response's audio_content is binary.
-fpth_output = "./data/output.mp3"
-with open(fpth_output, "wb") as out:
-    # Write the response to the output file.
-    out.write(response.audio_content)
-    print(f'Audio content written to file {fpth_output}.')
+        # Build the voice request, select the language code ("en-US") and the ssml
+        # voice gender ("neutral")
+        voice = texttospeech.VoiceSelectionParams(
+            language_code=self.lang_code[lang],
+            ssml_gender=self.gender_type[gender]
+        )
+
+        # Select the type of audio file you want returned
+        audio_config = texttospeech.AudioConfig(
+            audio_encoding=texttospeech.AudioEncoding.MP3
+        )
+
+        # Perform the text-to-speech request on the text input with the selected
+        # voice parameters and audio file type
+        self.response = self.client.synthesize_speech(
+            input=synthesis_input, voice=voice, audio_config=audio_config
+        )
+
+    def save_audio(self, fpth_output='./data/output.mp3'):
+        # The response's audio_content is binary.
+        with open(fpth_output, "wb") as out:
+            # Write the response to the output file.
+            out.write(self.response.audio_content)
+            print(f'Audio content written to file {fpth_output}.')
+
+def main():
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './data/secret.json'
+
+
+    text = "こんにちは，わたしは山田太郎です"
+    lang = '日本語'
+    gender = 'default'
+
+    txt2speech = Txt2Speech()
+    txt2speech.synthesize_speech(text, lang, gender)
+    txt2speech.save_audio()
+
+
+if __name__ == '__main__':
+    main()
