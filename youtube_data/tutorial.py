41424bf76770a6fd73ac520c245124b90ee09103
ne745 <kkpma0@gmail.com>
2022-04-03 21:36:05 +0900

    動画のみを検索

--- youtube_data/tutorial.py
+++ youtube_data/tutorial.py
@@ -8,8 +8,8 @@ with open('./data/secret.json') as f:
 
 
 DEVELOPER_KEY = secret['KEY']
-YOUTUBE_API_SERVICE_NAME = "youtube"
-YOUTUBE_API_VERSION = "v3"
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
 
 youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
 
@@ -20,10 +20,13 @@ max_results = 10
 # query term.
 response = youtube.search().list(
     q=q,
-    part="id,snippet",
+    part='id,snippet',
+    order='viewCount',
+    type='video',
     maxResults=max_results
 ).execute()
 
-item = response['items']
-print(item[0])
+items = response['items']
+item = items[0]
+print(item)
 print(len(item))
