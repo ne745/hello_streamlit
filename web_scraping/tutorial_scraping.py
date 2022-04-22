4bdbb6dc7c6fc099e1361035f534fadaec445649
ne745 <kkpma0@gmail.com>
2022-04-22 21:40:19 +0900

    ECサイトの情報の取得

--- web_scraping/tutorial_scraping.py
+++ web_scraping/tutorial_scraping.py
@@ -14,4 +14,16 @@ n_subscriber = int(n_subscriber.split('：')[1])
 n_review = soup.find('p', {'class': 'reviews'}).text
 n_review = int(n_review.split('：')[1])
 
-print(n_subscriber, n_review)
+url_ec = 'https://scraping.official.ec/'
+respons = requests.get(url_ec)
+soup = BeautifulSoup(respons.text, 'html.parser')
+
+item_list = soup.find('ul', {'id': 'itemList'})
+items = item_list.find_all('li')
+
+for item in items:
+    title = item.find('p', {'class': 'items-grid_itemTitleText_5a0255a1'}).text
+    price = item.find('p', {'class': 'items-grid_price_5a0255a1'}).text
+    price = int(price.replace('¥', '').replace(',', '').replace(' ', ''))
+    link = item.find('a')['href']
+    is_stock = '在庫あり' if item.find('p', {'class': 'items-grid_soldOut_5a0255a1'}) is None else '在庫なし'
